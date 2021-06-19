44a4d6ee5625c27e2cfc7cb4f97bbbd17ee31070
Benjamin Looi <benjaminlooidt@gmail.com>
2021-06-19 15:49:50 +0800

    Improve google lighthouse

--- src/components/SocialIcons/index.js
+++ src/components/SocialIcons/index.js
@@ -12,26 +12,46 @@ import * as style from "./socialIcons.module.scss"
 export default function SocialIcons() {
   return (
     <ul className={style.socialIconsRow}>
-      <a className="p-2 fa-lg" href="https://www.facebook.com/benjaminlooi97">
+      <a
+        className="p-2 fa-lg"
+        href="https://www.facebook.com/benjaminlooi97"
+        aria-label="Benjamin Looi's Facebook"
+      >
         <FontAwesomeIcon
           icon={faFacebookSquare}
           className={style.facebookIcon}
         />
       </a>
 
-      <a className="p-2 fa-lg" href="https://www.instagram.com/benjaminlooi/">
+      <a
+        className="p-2 fa-lg"
+        href="https://www.instagram.com/benjaminlooi/"
+        aria-label="Benjamin Looi's Instagram"
+      >
         <FontAwesomeIcon icon={faInstagram} className={style.instagramIcon} />
       </a>
 
-      <a className="p-2 fa-lg" href="https://github.com/Benjaminlooi">
+      <a
+        className="p-2 fa-lg"
+        href="https://github.com/Benjaminlooi"
+        aria-label="Benjamin Looi's Github"
+      >
         <FontAwesomeIcon icon={faGithub} className={style.githubIcon} />
       </a>
 
-      <a className="p-2 fa-lg" href="https://twitter.com/benjaminlooi_">
+      <a
+        className="p-2 fa-lg"
+        href="https://twitter.com/benjaminlooi_"
+        aria-label="Benjamin Looi's Twitter"
+      >
         <FontAwesomeIcon icon={faTwitter} className={style.twitterIcon} />
       </a>
 
-      <a className="p-2 fa-lg" href="https://www.linkedin.com/in/benjaminlooi/">
+      <a
+        className="p-2 fa-lg"
+        href="https://www.linkedin.com/in/benjaminlooi/"
+        aria-label="Benjamin Looi's Linked-in"
+      >
         <FontAwesomeIcon icon={faLinkedinIn} className={style.linkedinIcon} />
       </a>
     </ul>
